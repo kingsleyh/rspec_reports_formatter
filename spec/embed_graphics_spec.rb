8ec8bdaa02b73dfa93a8343360d82bcd36ebba1b
Vishal Banthia <vishal.banthia.vb@gmail.com>
2017-06-21 00:57:51 -0700

    add test

---
+++ spec/embed_graphics_spec.rb
@@ -0,0 +1,137 @@
+require 'rspec'
+require_relative '../lib/rspec_html_formatter'
+
+describe 'Embed Graphics' do
+  context 'Screenshots' do
+    it 'pass with screenshots' do |example|
+      example.metadata[:screenshots] = []
+
+      example.metadata[:screenshots] << {
+        caption: 'Playstore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+      example.metadata[:screenshots] << {
+        caption: 'Calculator1',
+        path: '../spec/support/assets/images/screen_2.png'
+      }
+      example.metadata[:screenshots] << {
+        caption: 'Calculator2',
+        path: '../spec/support/assets/images/screen_2.png'
+      }
+      example.metadata[:screenshots] << {
+        caption: 'Calculator3',
+        path: '../spec/support/assets/images/screen_2.png'
+      }
+      example.metadata[:screenshots] << {
+        caption: 'Paystore2',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+
+      expect(true).to eq(true)
+    end
+
+    it 'fail with screenshots' do |example|
+      example.metadata[:screenshots] = []
+
+      example.metadata[:screenshots] << {
+        caption: 'Playstore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+      example.metadata[:screenshots] << {
+        caption: 'Paystore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+
+      expect(true).to eq(false)
+    end
+
+    xit 'pending with screenshots' do |example|
+      example.metadata[:screenshots] = []
+
+      example.metadata[:screenshots] << {
+        caption: 'Playstore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+      example.metadata[:screenshots] << {
+        caption: 'Paystore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+
+      expect(true).to eq(false)
+    end
+  end
+
+  context 'Screenrecord' do
+    it 'pass with screenrecord' do |example|
+      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+
+      expect(true).to eq(true)
+    end
+
+    it 'fail with screenrecord' do |example|
+      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+
+      expect(true).to eq(false)
+    end
+
+    xit 'pending with screenrecord' do |example|
+      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+
+      expect(true).to eq(false)
+    end
+  end
+
+  context 'Screenrecord & Screenshot' do
+    it 'pass with screenrecord & screenshot' do |example|
+      example.metadata[:screenshots] = []
+
+      example.metadata[:screenshots] << {
+        caption: 'Playstore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+      example.metadata[:screenshots] << {
+        caption: 'Paystore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+
+      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+
+      expect(true).to eq(true)
+    end
+
+    it 'fail with screenrecord & screenshot' do |example|
+      example.metadata[:screenshots] = []
+
+      example.metadata[:screenshots] << {
+        caption: 'Playstore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+      example.metadata[:screenshots] << {
+        caption: 'Paystore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+
+      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+
+      expect(true).to eq(false)
+    end
+
+    xit 'pending with screenrecord & screenshot' do |example|
+      example.metadata[:screenshots] = []
+
+      example.metadata[:screenshots] << {
+        caption: 'Playstore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+      example.metadata[:screenshots] << {
+        caption: 'Paystore1',
+        path: '../spec/support/assets/images/screen_1.png'
+      }
+
+      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+
+      expect(true).to eq(false)
+    end
+  end
+
+end
