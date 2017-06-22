62dad49e20f6dbd5dd9942cb98c3ab71fd31c43c
Vishal Banthia <vishal.banthia.vb@gmail.com>
2017-06-22 15:01:34 -0700

    move assets to report holder beofre test

--- spec/embed_graphics_spec.rb
+++ spec/embed_graphics_spec.rb
@@ -1,29 +1,35 @@
 require 'rspec'
 
 describe 'Embed Graphics' do
+
+  before(:all) do
+    # Move assets to report folder
+
+    FileUtils.cp './spec/support/assets/images/screen_1.png', './rspec_html_reports/screenshots/'
+    FileUtils.cp './spec/support/assets/images/screen_2.png', './rspec_html_reports/screenshots/'
+    FileUtils.cp './spec/support/assets/videos/screen_record.mp4', './rspec_html_reports/screenrecords/'
+
+  end
+
   context 'Screenshots' do
     it 'pass with screenshots' do |example|
       example.metadata[:screenshots] = []
 
       example.metadata[:screenshots] << {
         caption: 'Playstore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: './screenshots/screen_1.png'
       }
       example.metadata[:screenshots] << {
         caption: 'Calculator1',
-        path: '../spec/support/assets/images/screen_2.png'
+        path: './screenshots/screen_2.png'
       }
       example.metadata[:screenshots] << {
         caption: 'Calculator2',
-        path: '../spec/support/assets/images/screen_2.png'
+        path: './screenshots/screen_2.png'
       }
       example.metadata[:screenshots] << {
         caption: 'Calculator3',
-        path: '../spec/support/assets/images/screen_2.png'
-      }
-      example.metadata[:screenshots] << {
-        caption: 'Paystore2',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: './screenshots/screen_1.png'
       }
 
       expect(true).to eq(true)
@@ -34,11 +40,11 @@ describe 'Embed Graphics' do
 
       example.metadata[:screenshots] << {
         caption: 'Playstore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_1.png'
       }
       example.metadata[:screenshots] << {
         caption: 'Paystore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_2.png'
       }
 
       expect(true).to eq(false)
@@ -49,11 +55,11 @@ describe 'Embed Graphics' do
 
       example.metadata[:screenshots] << {
         caption: 'Playstore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_1.png'
       }
       example.metadata[:screenshots] << {
         caption: 'Paystore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_2.png'
       }
 
       expect(true).to eq(false)
@@ -62,19 +68,19 @@ describe 'Embed Graphics' do
 
   context 'Screenrecord' do
     it 'pass with screenrecord' do |example|
-      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+      example.metadata[:screenrecord] = '../rspec_html_reports/screenrecords/screen_record.mp4'
 
       expect(true).to eq(true)
     end
 
     it 'fail with screenrecord' do |example|
-      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+      example.metadata[:screenrecord] = '../rspec_html_reports/screenrecords/screen_record.mp4'
 
       expect(true).to eq(false)
     end
 
     xit 'pending with screenrecord' do |example|
-      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+      example.metadata[:screenrecord] = '../rspec_html_reports/screenrecords/screen_record.mp4'
 
       expect(true).to eq(false)
     end
@@ -86,14 +92,14 @@ describe 'Embed Graphics' do
 
       example.metadata[:screenshots] << {
         caption: 'Playstore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_1.png'
       }
       example.metadata[:screenshots] << {
         caption: 'Paystore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_1.png'
       }
 
-      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+      example.metadata[:screenrecord] = '../rspec_html_reports/screenrecords/screen_record.mp4'
 
       expect(true).to eq(true)
     end
@@ -103,14 +109,14 @@ describe 'Embed Graphics' do
 
       example.metadata[:screenshots] << {
         caption: 'Playstore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_1.png'
       }
       example.metadata[:screenshots] << {
         caption: 'Paystore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_1.png'
       }
 
-      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+      example.metadata[:screenrecord] = '../rspec_html_reports/screenrecords/screen_record.mp4'
 
       expect(true).to eq(false)
     end
@@ -120,14 +126,14 @@ describe 'Embed Graphics' do
 
       example.metadata[:screenshots] << {
         caption: 'Playstore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_1.png'
       }
       example.metadata[:screenshots] << {
         caption: 'Paystore1',
-        path: '../spec/support/assets/images/screen_1.png'
+        path: '../rspec_html_reports/screenshots/screen_1.png'
       }
 
-      example.metadata[:screenrecord] = '../spec/support/assets/videos/screen_record.mp4'
+      example.metadata[:screenrecord] = '../rspec_html_reports/screenrecords/screen_record.mp4'
 
       expect(true).to eq(false)
     end
